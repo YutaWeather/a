0dee3d96463e9572ebc1f4f1e1a1850414e5c5cb
Yuta Fujii <yutayutakid21@gmail.com>
2022-06-05 14:07:47 +0900

    あいうえお

--- a/ViewController.swift
+++ a/ViewController.swift
@@ -11,6 +11,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        print("あいうえお")
         // Do any additional setup after loading the view.
     }
 
